da6b978ca9f8c6f4d0dea885a8c38ed008b918c8
Ohidur Rahman Bappy <ohidurbappy@gmail.com>
2021-09-04 00:36:21 +0600

    new script

---
+++ python/stream_video.py
@@ -0,0 +1,37 @@
+#Import necessary libraries
+from flask import Flask, render_template, Response
+import cv2
+app = Flask(__name__)
+
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
+
+
+camera = cv2.VideoCapture(0)
+# camera=cv2.VideoCapture('video.mp4')
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/')
+def index():
+    return render_template('streaming.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8082,debug=True)
+
